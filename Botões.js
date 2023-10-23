83c1414ce6a83d5967fa23ecd7b7b393905e1434
Volcarona433 <54645459+Volcarona433@users.noreply.github.com>
2023-10-23 18:48:54 -0300

    Add files via upload

---
+++ Botões.js
@@ -0,0 +1,41 @@
+function abrirPerfil(){
+    document.getElementById('Barra-Menu-Link')
+}
+
+function abrirPesquisa(){
+    document.getElementById('Barra-Menu-Link')
+}
+
+function abrirFavoritos(){
+    document.getElementById('Barra-Menu-Link')
+}
+
+function abrirBiblioteca(){
+    document.getElementById('Barra-Menu-Link')
+}
+
+function abrirCarteira(){
+    document.getElementById('Barra-Menu-Link')
+
+
+
+}function abrirPerfil(){
+    document.getElementById('Barra-Menu-Link')
+}
+
+function abrirPerfil(){
+    document.getElementById('Barra-Menu-Link')
+}
+
+function abrirPerfil(){
+    document.getElementById('Barra-Menu-Link')
+}
+
+function abrirPerfil(){
+    document.getElementById('Barra-Menu-Link')
+}
+
+function abrirPerfil(){
+    document.getElementById('Barra-Menu-Link')
+}
+
